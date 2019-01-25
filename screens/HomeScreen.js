3791536e4676a0118f6277a97c093bf5e2b5fc2f
AshleyAbramowiczGibbs <ashleyabramo2@gmail.com>
2019-01-24 19:40:54 -0600

    Add Photo to todo App

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -15,21 +15,9 @@ export default class HomeScreen extends React.Component {
         <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
           <View style={styles.welcomeContainer}>
             <Image
-              source={__DEV__ ? require("../assets/images/robot-dev.png") : require("../assets/images/robot-prod.png")}
+              source={__DEV__ ? require("../assets/images/todo.png") : require("../assets/images/robot-prod.png")}
               style={styles.welcomeImage}
             />
-          </View>
-
-          <View style={styles.getStartedContainer}>
-            {this._maybeRenderDevelopmentModeWarning()}
-
-            <Text style={styles.getStartedText}>more textGet started by opening</Text>
-
-            <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-              <MonoText style={styles.codeHighlightText}>this isaidaosidu oasid screens/HomeScreen.js</MonoText>
-            </View>
-
-            <Text style={styles.getStartedText}>Change this text and your app will automatically reload.</Text>
           </View>
 
           <View style={styles.helpContainer}>
